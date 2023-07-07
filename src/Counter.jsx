855b4bc1e56d7b55d07ef72c459db80295616470
Zain Fathoni <me@zainf.dev>
2023-07-07 20:20:11 +0800

    feat: Add Counter component and countAtom state

---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useAtom } from "jotai";
+import { countAtom } from "./state";
+
+function Counter() {
+  const [count, setCount] = useAtom(countAtom);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
